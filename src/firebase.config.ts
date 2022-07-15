9971ed54e8c0361836f840c4ec257cb3d03b12db
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-07-16 00:04:54 +0530

    fix: custom auth init to save data

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  initializeAuth,
+  browserLocalPersistence,
+} from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import type { CollectionReference, DocumentData } from "firebase/firestore";
 import { collection, getFirestore } from "firebase/firestore";
@@ -25,8 +28,9 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
+export const auth = initializeAuth(app, {
+  persistence: browserLocalPersistence,
+});
 export const firestore = getFirestore(app);
 export const storage = getStorage(app);
 
